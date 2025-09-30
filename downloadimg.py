4c9bbc295f3585d29ea56a8ec8dcbfebf815ada6
BlockHaity <104670904+BlockHaity@users.noreply.github.com>
2025-09-30 13:25:55 +0800

    修改API文件目录格式

--- downloadimg.py
+++ downloadimg.py
@@ -1,4 +1,3 @@
-from urllib.request import urlretrieve
 import json
 import os
 import subprocess
@@ -11,8 +10,10 @@ if __name__ == '__main__':
                 data = json.load(f)
                 for i in data:
                     localdir = i['local'].split('https://api-vercel.blockhaity.dpdns.org/')[1]
-                    print('downloading '+i['source']+' to '+localdir)
-                    # urlretrieve(i['source'],localdir)
-                    status = subprocess.run(['aria2c', '-o', localdir, i['source']])
-                    if status.returncode != 0:
-                        raise Exception('download failed')
+                    if os.path.exists(localdir) is not True:
+                        print('downloading '+i['source']+' to '+localdir)
+                        status = subprocess.run(['aria2c', '-o', localdir, i['source']])
+                        if status.returncode != 0:
+                            raise Exception('download failed')
+                    else:
+                        print("skip download "+ i['source'])
